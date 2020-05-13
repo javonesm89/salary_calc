187b67fc1da0f58b1f6a5a4a94e4d4fbdd41de52
Javone <javosmith12@gmail.com>
2020-05-13 15:19:03 -0400

    adding if/else control flow with methods

--- bin/app_salary_calculator.rb
+++ bin/app_salary_calculator.rb
@@ -1,19 +1,15 @@
 path = "/Users/javonesmith/Post_Flatiron/Prac_Ruby/salary_calc/lib/salary_calculator.rb"
 require_relative path
-
 require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 puts "Hey! Let's make YOU some money!"
-
 sleep(1)
 
 name = prompt.ask("What's your name?")
-
 sleep(0.5)
 
 puts "#{name}, let's get paid!"
-
 sleep(0.5)
 
 puts "But first #{name}, I need a litte info from you."
@@ -27,22 +23,36 @@ else
     sleep(0.5)
     abort
 end
-
 sleep(0.5)
 
 
-state = prompt.ask("What state do live in or intend to live in? (Ex. 'NY', 'TX')", required: true) do |q|
+state = prompt.ask("What state do live in or intend to live in? (Ex. NY, TX)", required: true) do |q|
     q.modify :up
 end
 
 puts "I heard #{state} is nice around this time of year."
 
-sleep(0.5)
+if (income_tax?(state))
+    puts "Unfortunately, #{state} is an income state."
+elsif (no_income_tax?(state))
+    puts "Fortunately, #{state} is not an income state."
+elsif (interest_dividends?(state))
+    puts "Hm. So, #{state} is not an income state but there's a catch." 
+    sleep(0.3)
+    puts "You will have to pay interest on dividends in the state of #{state}."
+end
 
-career = prompt.ask("What do you do for a living or plan on doing?")
-salary = prompt.ask("Ok, and how are they paying you or offering you?")
+puts ""
 
-puts "Mm, thanks."
+# end ? "It's here!" : "No it's not"
+# puts outcome
+
+# sleep(0.5)
+
+# career = prompt.ask("What do you do for a living or plan on doing?")
+# salary = prompt.ask("Ok, and how are they paying you or offering you?")
+
+# puts "Mm, thanks."
 
 # salary_calc(career,salary)
 
--- lib/salary_calculator.rb
+++ lib/salary_calculator.rb
@@ -1,1 +1,37 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+def income_tax?(state)
+
+    new_state = state.to_s
+    income_tax = ["IL", "IN", "MA", "MI", "NC", "PA", "UT", "CO","CA", "HI", "OR", "MN", "IA", "NJ", "VT", "DC", "NY", "WI"]
+    if (income_tax.find {|i| i == new_state})
+        return new_state
+    end
+end
+
+def no_income_tax?(state)
+     no_income_tax = ["AK", "FL", "NV", "SD", "TX", "WA", "WY"]
+     if (no_income_tax.find {|n| n == state})
+        return state
+    end
+end
+
+def interest_dividends?(state)
+
+    interest_dividends = ["TN", "NH"]
+    if (interest_dividends.find {|n| n == state})
+        return state
+    end
+
+end

38c0d38c6bb5857c180dedd67ab3cefc721c048c
Javone <javosmith12@gmail.com>
2020-05-13 16:15:17 -0400

    Update app_salary_calculator.rb

--- bin/app_salary_calculator.rb
+++ bin/app_salary_calculator.rb
@@ -3,6 +3,10 @@ require_relative path
 require 'tty-prompt'
 prompt = TTY::Prompt.new
 
+puts "FULL DISCLOSURE!"
+puts "ALL CALCULATIONS ARE BASED OFF SINGLE FILER (E.G, 'single man', 'single woman')"
+sleep(2)
+
 puts "Hey! Let's make YOU some money!"
 sleep(1)
 
@@ -12,37 +16,42 @@ sleep(0.5)
 puts "#{name}, let's get paid!"
 sleep(0.5)
 
-puts "But first #{name}, I need a litte info from you."
+puts "But first #{name}, I need a litte more info from you."
 
 answer = prompt.yes?("Sounds Good?")
 
 if (answer == true)
-    puts "Woot Woot 😎"
+    puts "Woot Woot 💰"
 else 
-    puts "Oh, ok. I guess next time 😟"
+    puts "Oh, ok. I guess next time ✌️"
     sleep(0.5)
     abort
 end
 sleep(0.5)
 
+career = prompt.ask("What are you currently doing or plan on doing for your career?", require: true)
 
 state = prompt.ask("What state do live in or intend to live in? (Ex. NY, tx)", required: true) do |q|
     q.modify :up
 end
-
-puts "I heard #{state} is nice around this time of year."
+puts "You're a #{career} in #{state}? Nice! Keep it up."
+sleep(1)
 
 if (income_tax?(state))
-    puts "Unfortunately, #{state} is an income state."
+    puts "Unfortunately, #{state} is a tax income state. 😕"
 elsif (no_income_tax?(state))
-    puts "Fortunately, #{state} is not an income state."
+    puts "Fortunately, #{state} is not a tax income state. 😎"
 elsif (interest_dividends?(state))
-    puts "Hm. So, #{state} is not an income state but there's a catch." 
-    sleep(0.3)
+    puts "Hm. So, #{state} is not an tax income state but there's a catch. 🤔" 
+    sleep(0.5)
     puts "You will have to pay interest on dividends in the state of #{state}."
 end
 
-puts ""
+puts "Now #{name}, let's see how much you'll take home in the state of #{state}. "
+sleep(1)
+
+prompt.
+
 
 # end ? "It's here!" : "No it's not"
 # puts outcome
